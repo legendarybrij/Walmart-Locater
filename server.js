20a3315457aa23a343d4edfb17b13473b22969af
Brijesh Patel <Brij@Brijesh-Patels-MacBook-Pro.local>
2019-05-13 22:44:48 -0400

    finished mapping

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+const cors = require("cors");
 var db = require("./models");
 
 var app = express();
@@ -11,6 +11,13 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': '*',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
 
 // Handlebars
 app.engine(
