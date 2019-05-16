97cde100aa6c43df24d83166d44a3ac9689d587e
Brijesh Patel <Brij@Brijesh-Patels-MacBook-Pro.local>
2019-05-15 20:25:29 -0400

    edited index.js js file

--- public/js/index.js
+++ public/js/index.js
@@ -133,114 +133,6 @@ var categoryPoint = { "infants&toddler":
   {"x":"678px", "y":"464px"}                   
 };
 
-	
-	
-	
-	
-	
-	
-
-// The API object contains methods for each kind of request we'll make
-// var API = {
-//   saveExample: function(example) {
-//     return $.ajax({
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       type: "POST",
-//       url: "api/examples",
-//       data: JSON.stringify(example)
-//     });
-//   },
-//   getExamples: function() {
-//     return $.ajax({
-//       url: "api/examples",
-//       type: "GET"
-//     });
-//   },
-//   deleteExample: function(id) {
-//     return $.ajax({
-//       url: "api/examples/" + id,
-//       type: "DELETE"
-//     });
-//   }
-// };
-
-// refreshExamples gets new examples from the db and repopulates the list
-// var refreshExamples = function() {
-//   API.getExamples().then(function(data) {
-//     var $examples = data.map(function(example) {
-//       var $a = $("<a>")
-//         .text(example.text)
-//         .attr("href", "/example/" + example.id);
-
-//       var $li = $("<li>")
-//         .attr({
-//           class: "list-group-item",
-//           "data-id": example.id
-//         })
-//         .append($a);
-
-//       var $button = $("<button>")
-//         .addClass("btn btn-danger float-right delete")
-//         .text("ｘ");
-
-//       $li.append($button);
-
-//       return $li;
-//     });
-
-//     $exampleList.empty();
-//     $exampleList.append($examples);
-//   });
-// };
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim()
-    // description: $exampleDescription.val().trim()
-  };
-
-  // if (!(example.text && example.description)) {
-    if (!(example.text)) {
-    alert("You must enter an example text!");
-    return;
-  }
-
-  // API.saveExample(example).then(function() {
-  //   refreshExamples();
-  // });
-
-  $exampleText.val("");
-  //$exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-// var handleDeleteBtnClick = function() {
-//   var idToDelete = $(this)
-//     .parent()
-//     .attr("data-id");
-
-//   API.deleteExample(idToDelete).then(function() {
-//     refreshExamples();
-//   });
-// };
-
-// Add event listeners to the submit and delete buttons
-//$submitBtn.on("click", handleFormSubmit);
-//$exampleList.on("click", ".delete", handleDeleteBtnClick);
-
-// ////TO GET MAP COORDINATES///
-// $mapdivcordinates.on("click",function(event){
-//   console.log("X = "+event.originalEvent.offsetX +" Y = "+event.originalEvent.offsetY);
-  
-// });
-// ////TO GET MAP COORDINATES///
 
 function searchWalmart(search) {
   $items.empty();
@@ -300,19 +192,14 @@ function searchWalmart(search) {
         }else{
         stock.css({"padding-bottom": "30px"});
         }
-        //category
-        //changing Pin
-        // imgBox.on("click",function(event){
-          
-        //   $(".pin1").css({"left": });
-        //   $(".pin1").css({"top": });
-        // });
+     
+      
         
         //Finding category
         imgBox.on("click",function(event){
           $(".pin2").hide();
           var dataID= event.currentTarget.attributes[2].textContent.split("/");
-          //console.log(dataID);
+          console.log(dataID);
           $(".pin1").show();
           for(i in categoryPoint)
           {
@@ -588,6 +475,10 @@ function searchWalmart(search) {
                 { 
                   $(".pin1").css({"left": categoryPoint["automotive"]["x"]});
                   $(".pin1").css({"top": categoryPoint["automotive"]["y"]});
+                }else if(dataID[0]==="sports&outdoors")
+                { 
+                  $(".pin1").css({"left": categoryPoint["sportinggoods"]["x"]});
+                  $(".pin1").css({"top": categoryPoint["sportinggoods"]["y"]});
                 }
             
           }
